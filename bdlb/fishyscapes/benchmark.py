3d01055c0f309352ffe9660ea73b6182c7a151b5
Krzysztof Lis <adynathos@gmail.com>
2020-10-09 20:33:29 +0200

    split AP curve function for reuse

--- bdlb/fishyscapes/benchmark.py
+++ bdlb/fishyscapes/benchmark.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+from typing import List
 from tqdm import tqdm
 import tensorflow_datasets as tfds
 
@@ -119,88 +120,97 @@ class FishyscapesValidation(Benchmark):
             labels.append(batch['mask'].numpy())
             uncertainties.append(estimator(batch['image_left']).numpy())
 
-        # concatenate lists for labels and uncertainties together
-        if (labels[0].shape[-1] > 1 and np.ndim(labels[0]) > 2) or \
-                (labels[0].shape[-1] == 1 and np.ndim(labels[0]) > 3):
-            # data is already in batches
-            labels = np.concatenate(labels)
-            uncertainties = np.concatenate(uncertainties)
-        else:
-            labels = np.stack(labels)
-            uncertainties = np.stack(uncertainties)
-        labels = labels.squeeze()
-        uncertainties = uncertainties.squeeze()
+        return calculate_metrics_perpixAP(
+            labels, 
+            uncertainties, 
+            num_points=num_points,
+        )
+     
 
-        # NOW CALCULATE METRICS
-        pos = labels == 1
-        valid = np.logical_or(labels == 1, labels == 0)  # filter out void
-        gt = pos[valid]
-        del pos
-        uncertainty = uncertainties[valid].reshape(-1).astype(np.float32, copy=False)
-        del valid
+def calculate_metrics_perpixAP(labels : List[np.ndarray], uncertainties : List[np.ndarray], num_points=50):
 
-        # Sort the classifier scores (uncertainties)
-        sorted_indices = np.argsort(uncertainty, kind='mergesort')[::-1]
-        uncertainty, gt = uncertainty[sorted_indices], gt[sorted_indices]
-        del sorted_indices
+    # concatenate lists for labels and uncertainties together
+    if (labels[0].shape[-1] > 1 and np.ndim(labels[0]) > 2) or \
+            (labels[0].shape[-1] == 1 and np.ndim(labels[0]) > 3):
+        # data is already in batches
+        labels = np.concatenate(labels)
+        uncertainties = np.concatenate(uncertainties)
+    else:
+        labels = np.stack(labels)
+        uncertainties = np.stack(uncertainties)
+    labels = labels.squeeze()
+    uncertainties = uncertainties.squeeze()
 
-        # Remove duplicates along the curve
-        distinct_value_indices = np.where(np.diff(uncertainty))[0]
-        threshold_idxs = np.r_[distinct_value_indices, gt.size - 1]
-        del distinct_value_indices, uncertainty
+    # NOW CALCULATE METRICS
+    pos = labels == 1
+    valid = np.logical_or(labels == 1, labels == 0)  # filter out void
+    gt = pos[valid]
+    del pos
+    uncertainty = uncertainties[valid].reshape(-1).astype(np.float32, copy=False)
+    del valid
 
-        # Accumulate TPs and FPs
-        tps = np.cumsum(gt, dtype=np.uint64)[threshold_idxs]
-        fps = 1 + threshold_idxs - tps
-        del threshold_idxs
+    # Sort the classifier scores (uncertainties)
+    sorted_indices = np.argsort(uncertainty, kind='mergesort')[::-1]
+    uncertainty, gt = uncertainty[sorted_indices], gt[sorted_indices]
+    del sorted_indices
 
-        # Compute Precision and Recall
-        precision = tps / (tps + fps)
-        precision[np.isnan(precision)] = 0
-        recall = tps / tps[-1]
-        # stop when full recall attained and reverse the outputs so recall is decreasing
-        sl = slice(tps.searchsorted(tps[-1]), None, -1)
-        precision = np.r_[precision[sl], 1]
-        recall = np.r_[recall[sl], 0]
-        average_precision = -np.sum(np.diff(recall) * precision[:-1])
+    # Remove duplicates along the curve
+    distinct_value_indices = np.where(np.diff(uncertainty))[0]
+    threshold_idxs = np.r_[distinct_value_indices, gt.size - 1]
+    del distinct_value_indices, uncertainty
 
-        # select num_points values for a plotted curve
-        interval = 1.0 / num_points
-        curve_precision = [precision[-1]]
-        curve_recall = [recall[-1]]
-        idx = recall.size - 1
-        for p in range(1, num_points):
-            while recall[idx] < p * interval:
-                idx -= 1
-            curve_precision.append(precision[idx])
-            curve_recall.append(recall[idx])
-        curve_precision.append(precision[0])
-        curve_recall.append(recall[0])
-        del precision, recall
+    # Accumulate TPs and FPs
+    tps = np.cumsum(gt, dtype=np.uint64)[threshold_idxs]
+    fps = 1 + threshold_idxs - tps
+    del threshold_idxs
 
-        if tps.size == 0 or fps[0] != 0 or tps[0] != 0:
-            # Add an extra threshold position if necessary
-            # to make sure that the curve starts at (0, 0)
-            tps = np.r_[0., tps]
-            fps = np.r_[0., fps]
+    # Compute Precision and Recall
+    precision = tps / (tps + fps)
+    precision[np.isnan(precision)] = 0
+    recall = tps / tps[-1]
+    # stop when full recall attained and reverse the outputs so recall is decreasing
+    sl = slice(tps.searchsorted(tps[-1]), None, -1)
+    precision = np.r_[precision[sl], 1]
+    recall = np.r_[recall[sl], 0]
+    average_precision = -np.sum(np.diff(recall) * precision[:-1])
 
-        # Compute TPR and FPR
-        tpr = tps / tps[-1]
-        del tps
-        fpr = fps / fps[-1]
-        del fps
+    # select num_points values for a plotted curve
+    interval = 1.0 / num_points
+    curve_precision = [precision[-1]]
+    curve_recall = [recall[-1]]
+    idx = recall.size - 1
+    for p in range(1, num_points):
+        while recall[idx] < p * interval:
+            idx -= 1
+        curve_precision.append(precision[idx])
+        curve_recall.append(recall[idx])
+    curve_precision.append(precision[0])
+    curve_recall.append(recall[0])
+    del precision, recall
 
-        # Compute AUROC
-        auroc = np.trapz(tpr, fpr)
+    if tps.size == 0 or fps[0] != 0 or tps[0] != 0:
+        # Add an extra threshold position if necessary
+        # to make sure that the curve starts at (0, 0)
+        tps = np.r_[0., tps]
+        fps = np.r_[0., fps]
 
-        # Compute FPR@95%TPR
-        fpr_tpr95 = fpr[np.searchsorted(tpr, 0.95)]
+    # Compute TPR and FPR
+    tpr = tps / tps[-1]
+    del tps
+    fpr = fps / fps[-1]
+    del fps
 
-        return {
-            'auroc': auroc,
-            'AP': average_precision,
-            'FPR@95%TPR': fpr_tpr95,
-            'recall': np.array(curve_recall),
-            'precision': np.array(curve_precision),
-        }
+    # Compute AUROC
+    auroc = np.trapz(tpr, fpr)
+
+    # Compute FPR@95%TPR
+    fpr_tpr95 = fpr[np.searchsorted(tpr, 0.95)]
+
+    return {
+        'auroc': auroc,
+        'AP': average_precision,
+        'FPR@95%TPR': fpr_tpr95,
+        'recall': np.array(curve_recall),
+        'precision': np.array(curve_precision),
+    }
 
